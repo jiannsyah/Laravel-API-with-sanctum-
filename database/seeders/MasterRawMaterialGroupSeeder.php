9e6dae16ff9852a604108a663ea10307b766dfd0
jiannsyah <brian8jian@gmail.com>
2024-10-28 05:47:07 +0700

    seeder masterRawMaterialGroup

--- database/seeders/MasterRawMaterialGroupSeeder.php
+++ database/seeders/MasterRawMaterialGroupSeeder.php
@@ -18,6 +18,18 @@ class MasterRawMaterialGroupSeeder extends Seeder
             'id' => Str::uuid()->toString(),    // Mengisi UUID secara manual
             'codeRawMaterialGroup' => '10001',
             'nameRawMaterialGroup' => 'TAPIOKA',
+            'unitOfMeasurement' => 'KG',
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'codeRawMaterialType' => 'd7dbd3f7-cfa3-4e5e-9e8a-9002d0a86daa'
+        ]);
+        DB::table('master_raw_material_groups')->insert([
+            'id' => Str::uuid()->toString(),    // Mengisi UUID secara manual
+            'codeRawMaterialGroup' => '10002',
+            'nameRawMaterialGroup' => 'AREN',
+            'unitOfMeasurement' => 'KG',
             'created_by' => 1,
             'updated_by' => 1,
             'created_at' => now(),
