89589114d9d8772aad5c56a74215b5b58663cab6
jiannsyah <brian8jian@gmail.com>
2024-11-04 10:10:10 +0700

    masterRawMaterial |pa m:name --model=modelName --requests|pa m:rsc nameresource|pa m:rsc namecollection

---
+++ app/Http/Controllers/API/V1/Master/RawMaterial/MaterRawMaterialController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Master\RawMaterial;
+
+use App\Http\Controllers\Controller;
+use App\Models\MasterRawMaterial;
+use App\Http\Requests\StoreMasterRawMaterialRequest;
+use App\Http\Requests\UpdateMasterRawMaterialRequest;
+
+class MaterRawMaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMasterRawMaterialRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MasterRawMaterial $masterRawMaterial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MasterRawMaterial $masterRawMaterial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMasterRawMaterialRequest $request, MasterRawMaterial $masterRawMaterial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MasterRawMaterial $masterRawMaterial)
+    {
+        //
+    }
+}
